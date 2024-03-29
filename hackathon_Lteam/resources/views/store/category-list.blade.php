112a80740d39dc38e2c1a407f917aef67da171d0
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2024-03-29 10:04:14 +0900

    Merge branch 'main' of github.com:Ragamn/hackathon_Lteam

--- hackathon_Lteam/resources/views/store/category-list.blade.php
+++ hackathon_Lteam/resources/views/store/category-list.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/category-list-style.css">
+    <link rel="stylesheet" href="{{asset('/css/list-style.css')}}">
     <title>カテゴリーリスト</title>
 </head>
 <body>
--- hackathon_Lteam/resources/views/store/category.blade.php
+++ hackathon_Lteam/resources/views/store/category.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/category-style.css">
+    <link rel="stylesheet" href="{{asset('/css/category.css')}}">
     <title>カテゴリー編集</title>
 </head>
 <body>
